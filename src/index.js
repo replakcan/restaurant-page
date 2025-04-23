424ef815dc708b7855f441ad38db6c1e8a825248
replakcan <alper.makcan@gmail.com>
2025-04-23 23:06:36 +0300

    Adds eventListeners for navigation buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import ContactPage from './components/ContactPage'
 import HomePage from './components/HomePage'
+import MenuPage from './components/MenuPage'
+import { emptyDivContentById } from './lib/empty-div-content-by-id'
 import './reset.css'
 import './styles.css'
 
 HomePage()
+
+document.querySelector('button[id^="home"]').addEventListener('click', () => {
+  emptyDivContentById('content')
+
+  HomePage()
+})
+
+document.querySelector('button[id^="menu"]').addEventListener('click', () => {
+  emptyDivContentById('content')
+
+  MenuPage()
+})
+
+document.querySelector('button[id^="contact"]').addEventListener('click', () => {
+  emptyDivContentById('content')
+
+  ContactPage()
+})
